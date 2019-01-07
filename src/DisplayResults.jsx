a36f69753c167006d6ec69baba4808b17bbe6deb
Gregory Kallai <gerg.kallai@gmail.com>
2019-01-07 16:42:04 +0100

    Connects the caclulator with the main app, calculation wokring without button or styling

--- src/DisplayResults.jsx
+++ src/DisplayResults.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import { bmiCalculation } from './BMICalculator'
 
 class DisplayResults extends Component {
     
     calculate () {
         const weight = this.props.weight;
         const height = this.props.height;
+
+        return bmiCalculation(weight, height);
     }
 
     render () {
