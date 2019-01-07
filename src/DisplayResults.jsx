c6158f8e7877ab0a896f7083c907905149d59bd9
Gregory Kallai <gerg.kallai@gmail.com>
2019-01-07 16:38:06 +0100

    Adds bmicalculator js file with the bmi calculation logic, adds weight and height parameters to display results component

--- src/DisplayResults.jsx
+++ src/DisplayResults.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 
 class DisplayResults extends Component {
+    
+    calculate () {
+        const weight = this.props.weight;
+        const height = this.props.height;
+    }
 
     render () {
         return (
             <div id='resposne'>
+                {this.calculate()}
             </div>
         )
     }
