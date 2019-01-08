a4d173f07387ff802655c6c6a1d2f9f908cee008
Gregory Kallai <gerg.kallai@gmail.com>
2019-01-08 11:11:59 +0100

    Adds footer at a fixed position using the power of flexbox

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="border-2 bg-teal-lightest pin-b text-grey">
+            Created with React
+        </div>
+    )
+}
+
+export default Footer
