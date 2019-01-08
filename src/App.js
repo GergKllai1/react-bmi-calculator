8532ad4e2d240b34ddaf3e4c1830fdb19854a8b2
Gregory Kallai <gerg.kallai@gmail.com>
2019-01-08 10:31:28 +0100

    Adds the main bulk of styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import "./App.css";
-import './css/tailwind.css';
+import "./css/tailwind.css";
 import DisplayResults from "./DisplayResults";
+import { UndrawCalculator } from "react-undraw-illustrations";
 
 class App extends Component {
   constructor(props) {
@@ -23,12 +24,18 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
+      <div className="App text-center bg-grey-lightest h-screen font-sans text-lg text-grey-darkest">
+        <div className="bg-teal-lightest border-2 pb-2">
+          <UndrawCalculator />
+          <h2 className="">BMI Calculator</h2>
+        </div>
         <div>
           <label>
             Weight {this.state.methodName === "Metric" ? "(kgs)" : "(pounds)"}
           </label>
+          <br />
           <input
+            className="text-center bg-grey-light border-2 focus:bg-white p-1 rounded-lg"
             name="weight"
             value={this.state.weight}
             onChange={e => this.setState({ weight: e.target.value })}
@@ -38,13 +45,18 @@ class App extends Component {
           <label>
             Height {this.state.methodName === "Metric" ? "(cms)" : "(inches)"}
           </label>
+          <br />
           <input
+            className="text-center bg-grey-light focus:bg-white p-1 rounded-lg"
             name="weight"
             value={this.state.height}
             onChange={e => this.setState({ height: e.target.value })}
           />
           <br />
-          <button onClick={() => this.mehtodChange()}>
+          <button
+            className="p-2 m-2 w-1/6 b-2 border-2 hover:border-teal rounded-full bg-teal-light hover:bg-white  text-white hover:text-black font-bold"
+            onClick={() => this.mehtodChange()}
+          >
             {this.state.methodName}
           </button>
         </div>
--- src/DisplayResults.jsx
+++ src/DisplayResults.jsx
@@ -18,9 +18,13 @@ class DisplayResults extends Component {
 
     render() {
         return (
-        <div>
-            <button onClick={() => this.calculate()}>Caclulate BMI</button>
-            <div>{this.state.bmiMessage}</div>
+        <div className="p-4">
+            <button 
+            className="p-2 m-2 w-1/6 b-2 border-2 hover:border-teal rounded-full bg-teal-light hover:bg-white  text-white hover:text-black font-bold"
+            onClick={() => this.calculate()}>Caclulate BMI</button>
+            <div className="flex justify-center">
+                <div className="border-2 bg-teal-light p-4 rounded-lg text-white">{this.state.bmiMessage}</div>
+            </div>
         </div>
         );
     }
